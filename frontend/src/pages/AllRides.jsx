0b4a590167adb8bff90ae576bef60ca98787bbbe
titiksha008 <chughtitiksha@example.com>
2026-05-20 10:01:58 +0530

    deployment issues

--- frontend/src/pages/AllRides.jsx
+++ frontend/src/pages/AllRides.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Navbar from "../components/Navbar";
 import { useNavigate } from "react-router-dom";
-import "../styles/dashboard.css";
+import "../styles/Dashboard.css";
 
 const API = "https://cab-safety.onrender.com/api";
 
